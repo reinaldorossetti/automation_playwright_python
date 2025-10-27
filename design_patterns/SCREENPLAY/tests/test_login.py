0e1816299464bd9f6d054b7a1f19cbadd817c091
Reinaldo <reinaldo.rossetti@outlook.com>
2025-10-27 17:50:38 -0300

    testes com screnplay

--- design_patterns/SCREENPLAY/tests/test_login.py
+++ design_patterns/SCREENPLAY/tests/test_login.py
@@ -11,23 +11,18 @@ from actors.actor import prepare_actor_login_task, prepare_actor_login_question
 @allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.usefixtures("browser_type_launch_args")
 def test_ct001_realizar_login_com_sucesso(page: Page):
-    with allure.step("Preparar o ator com suas habilidades"):
-        # Arrange - Preparar o ator
+    with allure.step("# Arrange - Preparar o ator com suas habilidades"):
         login_task = prepare_actor_login_task(page)
         login_question = prepare_actor_login_question(page)
 
-    with allure.step("Acessar a página de login"):
-        # Act - Executar as ações como tasks
+    with allure.step("# Act - Acessar a página de login"):
         login_task.acessar_pagina_login()
 
-    with allure.step("Realizar login com usuário 'dan' e senha 'pwd'"):
-        # Act - Executar as ações como tasks
+    with allure.step("# Act - Realizar login com usuário 'dan' e senha 'pwd'"):
         login_task.realizar_login("dan", "pwd")
 
-    with allure.step("Verificar mensagem de boas-vindas"):
-        # Question - Verificar o resultado
+    with allure.step("# Assert - Verificar mensagem de boas-vindas"):
         login_question.mensagem_de_boas_vindas("dan")
 
-    with allure.step("Fechar a página"):
-        # Cleanup - Fechar a página
+    with allure.step("# Cleanup - Fechar a página"):
         login_task.fechar_pagina()
