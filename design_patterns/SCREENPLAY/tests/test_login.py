2e20054b8ae51a3cb48a671dd2e3059971fbbe95
Reinaldo <reinaldo.rossetti@outlook.com>
2025-10-27 17:45:44 -0300

    testes com screnplay

--- design_patterns/SCREENPLAY/tests/test_login.py
+++ design_patterns/SCREENPLAY/tests/test_login.py
@@ -1,22 +1,33 @@
 import pytest
+import allure
 from playwright.sync_api import Page
 from actors.actor import prepare_actor_login_task, prepare_actor_login_question
 
 
+@allure.feature("Autenticação")
+@allure.story("Login de Usuário")
+@allure.title("CT001 - Realizar login com sucesso")
+@allure.description("Verifica se o usuário consegue realizar login com credenciais válidas")
+@allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.usefixtures("browser_type_launch_args")
 def test_ct001_realizar_login_com_sucesso(page: Page):
-    # Arrange - Preparar o ator
-    login_task = prepare_actor_login_task(page)
-    login_question = prepare_actor_login_question(page)
+    with allure.step("Preparar o ator com suas habilidades"):
+        # Arrange - Preparar o ator
+        login_task = prepare_actor_login_task(page)
+        login_question = prepare_actor_login_question(page)
 
-    # Act - Executar as ações como tasks
-    login_task.acessar_pagina_login()
+    with allure.step("Acessar a página de login"):
+        # Act - Executar as ações como tasks
+        login_task.acessar_pagina_login()
 
-    # Act - Executar as ações como tasks
-    login_task.realizar_login("dan", "pwd")
+    with allure.step("Realizar login com usuário 'dan' e senha 'pwd'"):
+        # Act - Executar as ações como tasks
+        login_task.realizar_login("dan", "pwd")
 
-    # Question - Verificar o resultado
-    login_question.mensagem_de_boas_vindas("dan")
+    with allure.step("Verificar mensagem de boas-vindas"):
+        # Question - Verificar o resultado
+        login_question.mensagem_de_boas_vindas("dan")
 
-    # Cleanup - Fechar a página
-    login_task.fechar_pagina()
+    with allure.step("Fechar a página"):
+        # Cleanup - Fechar a página
+        login_task.fechar_pagina()
